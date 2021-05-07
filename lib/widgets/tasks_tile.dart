9505edee7a96365bc692f67040099d2d608d3cf8
me-aditya <adityarathi1212@gmail.com>
2021-05-07 17:43:52 +0530

    checked to strikethough :-) added

--- lib/widgets/tasks_tile.dart
+++ lib/widgets/tasks_tile.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   TaskTile({this.title});
   final String title;
 
   @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+
+  
+  bool checked = false;
+
+  var textStyle = TextStyle(color: Colors.black);
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(title),
+      title: Text(
+        widget.title,
+        style: textStyle,
+      ),
       trailing: Checkbox(
-          value: false,
+          value: checked,
           onChanged: (value) {
-            value = true;
+            setState(() {
+              checked = value;
+              if (value) {
+                textStyle = TextStyle(decoration: TextDecoration.lineThrough);
+              } else {
+                textStyle = TextStyle(color: Colors.black);
+              }
+            });
           }),
     );
   }
