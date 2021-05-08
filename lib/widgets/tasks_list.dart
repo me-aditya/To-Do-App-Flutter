b4ff55dfb572918e77c9f31bd6a0ba535c7c30e4
me-aditya <adityarathi1212@gmail.com>
2021-05-09 01:49:06 +0530

    IMP : changes for Consumer widget

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -4,24 +4,25 @@ import 'package:todo_list/widgets/tasks_tile.dart';
 import 'package:provider/provider.dart';
 
 class TaskList extends StatelessWidget {
-  
-
- 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return TaskTile(
-          title: Provider.of<TaskData>(context).tasks[index].name,
-          isChecked:Provider.of<TaskData>(context).tasks[index].isDone,
-          checkboxCallback: (checkBoxState) {
-            // setState(() {
-            //   Provider.of<TaskData>(context).tasks[index].toggleDone();
-            // });
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return TaskTile(
+              title: taskData.tasks[index].name,
+              isChecked: taskData.tasks[index].isDone,
+              checkboxCallback: (checkBoxState) {
+                // setState(() {
+                //   taskData.tasks[index].toggleDone();
+                // });
+              },
+            );
           },
+          itemCount: taskData.tasks.length,
         );
       },
-      itemCount: Provider.of<TaskData>(context).tasks.length,
     );
   }
 }
